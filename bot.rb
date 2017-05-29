6f896e2d386196fe11cdae785766b9ffe3da348a
Tom Sherman <the.tomsherman@gmail.com>
2017-05-29 21:58:18 +0100

    Logs are no longer sent to server by default

--- bot.rb
+++ bot.rb
@@ -30,18 +30,18 @@ end
 # Event handler for adding reputation, new thumbs up emoji = +1 rep
 @bot.reaction_add do |event|
   if event.emoji.name == '👍' && event.user.id != event.message.user.id
+    p "👍 #{event.user.name}##{event.user.id} repped #{event.message.user.name}##{event.message.user.id}"
     @db.execute('INSERT INTO reps (giver, receiver, message, channel, server) VALUES (?, ?, ?, ?, ?)',
                   [event.user.id, event.message.user.id, event.message.id,
                   event.channel.id, event.channel.server.id])
-    p "👍 #{event.user.name}##{event.user.id} repped #{event.message.user.name}##{event.message.user.id}"
   end
 end
 
 # Event handler for removing reputation
 @bot.reaction_remove do |event|
   if event.emoji.name == '👍' && event.user.id != event.message.user.id
-    @db.execute('DELETE FROM reps WHERE giver=? AND receiver=? and message=? and channel=? and server=? limit 1')
     p "#{event.user.name}##{event.user.id} reversed rep for #{event.message.user.name}##{event.message.user.id}"
+    @db.execute('DELETE FROM reps WHERE giver=? AND receiver=? and message=? and channel=? and server=? limit 1')
   end
 end
 
